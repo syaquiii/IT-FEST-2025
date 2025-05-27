9f480203226f24b8c78a9311ff7aad4738d25418
syaquiii <msyauqioqi@gmail.com>
2025-05-27 14:01:54 +0700

    chore : add event separator

--- src/feature/event/container/EventContainer.tsx
+++ src/feature/event/container/EventContainer.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../components/Hero";
 import Sponsor from "../components/Sponsor";
+import EventSeparator from "../components/EventSeparator";
 
 const EventContainer = () => {
   return (
     <section className="eventbg min-h-screen py-40 -mt-10  z-10 relative">
       <Hero />
       <Sponsor />
+      <EventSeparator />
     </section>
   );
 };
