2f318def04a8f321e5f0bc7913ea9bea33f9fe10
syaquiii <msyauqioqi@gmail.com>
2025-06-01 15:27:03 +0700

    feat : add admin structure page

---
+++ src/shared/_admin/data/nav-items.ts
@@ -0,0 +1,19 @@
+import { NavItem } from "../type/nav-item";
+
+export const navItems: NavItem[] = [
+  {
+    title: "Dashboard",
+    path: "/mangujo/admin",
+    icon: "dashboard",
+  },
+  {
+    title: "Team List",
+    path: "/mangujo/admin/team-list",
+    icon: "people",
+  },
+  {
+    title: "Announcement",
+    path: "/mangujo/admin/announcement",
+    icon: "event",
+  },
+];
