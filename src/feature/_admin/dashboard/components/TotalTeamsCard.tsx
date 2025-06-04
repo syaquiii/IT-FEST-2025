52d299ca7890ea8d5669d53fd6decd9c423df88d
syaquiii <msyauqioqi@gmail.com>
2025-06-04 19:04:02 +0700

    chore : add loading skeleton

--- src/feature/_admin/dashboard/components/TotalTeamsCard.tsx
+++ src/feature/_admin/dashboard/components/TotalTeamsCard.tsx
@@ -2,15 +2,25 @@ import React from "react";
 
 interface TotalTeamsCardProps {
   totalAll: number;
+  isLoading: boolean;
 }
 
-const TotalTeamsCard = ({ totalAll }: TotalTeamsCardProps) => {
+const TotalTeamsCard = ({ totalAll, isLoading }: TotalTeamsCardProps) => {
   return (
     <>
       <h4 className="text-2xl font-changa text-white font-bold">Total Teams</h4>
       <div className="w-full font-changa font-bold bg-slate-900 px-6 py-4 rounded-2xl outline-2 outline-blue-800 text-white mt-3">
-        <span className="text-3xl">{totalAll}</span>
-        <p className="font-normal text-lg">Total Teams</p>
+        {isLoading ? (
+          <>
+            <div className="h-9 w-16 bg-gray-700 rounded animate-pulse" />
+            <div className="h-7 w-28 bg-gray-700 rounded animate-pulse mt-1" />
+          </>
+        ) : (
+          <>
+            <span className="text-3xl">{totalAll}</span>
+            <p className="font-normal text-lg">Total Teams</p>
+          </>
+        )}
       </div>
     </>
   );
