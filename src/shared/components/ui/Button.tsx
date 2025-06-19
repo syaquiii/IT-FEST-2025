b3e03cf5bde5402ae9daace577bc481ad388ced9
syaquiii <msyauqioqi@gmail.com>
2025-06-20 01:51:03 +0700

    chore: add hover for button

--- src/shared/components/ui/Button.tsx
+++ src/shared/components/ui/Button.tsx
@@ -4,17 +4,17 @@ import { VariantProps, cva } from "class-variance-authority";
 import { cn } from "@/shared/utils/cn";
 
 const buttonVariants = cva(
-  "rounded-2xl flex justify-center items-center shrink-0 h-16  font-bold text-2xl ",
+  "rounded-2xl flex justify-center items-center shrink-0 h-16 font-bold text-2xl transition-all duration-300 ease-in-out cursor-pointer hover:scale-105 hover:shadow-lg active:scale-95",
   {
     variants: {
       variant: {
         primary:
-          "bg-button-bg text-button-text [text-shadow:_0px_0px_30px_rgba(255,255,255,0.6)] ",
+          "bg-button-bg text-button-text [text-shadow:_0px_0px_30px_rgba(255,255,255,0.6)] hover:bg-button-bg/50",
         secondary:
-          "bg-button-bg text-white  [text-shadow:_0px_0px_30px_rgba(255,255,255,0.6)]",
-        disabled: "bg-button-bg text-white text-[#4F5D6E]  ",
+          "bg-button-bg text-white [text-shadow:_0px_0px_30px_rgba(255,255,255,0.6)] hover:bg-button-bg/50",
+        disabled: "bg-button-bg text-white text-[#4F5D6E] cursor-not-allowed",
         tertiary:
-          "bg-transparent border-4  border-button-bg text-button-text [text-shadow:_0px_0px_30px_rgba(255,255,255,0.6)]",
+          "bg-transparent border-4 border-button-bg text-button-text [text-shadow:_0px_0px_30px_rgba(255,255,255,0.6)] hover:bg-button-bg hover:bg-opacity-20",
       },
       size: {
         small: "w-36",
